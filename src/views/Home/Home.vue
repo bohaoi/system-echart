a2cbe4fbff79e20e3051783a98cc05e3d5af3766
bohaoi <bohao8485@163.com>
2020-06-30 09:38:31 +0800

    second submit home

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -2,34 +2,40 @@
   <el-row class="home" :gutter="20">
     <el-col :span="8">
       <!--管理员-->
-      <el-card shadow="hover">
+      <el-card shadow="hover" style="height:290px">
         <!--管理员-->
         <div class="user">
           8
           <!-- <img :src="userImg"/> -->
         </div>
         <!--登录时间-->
-        <div>8</div>
+        <!-- <div>8</div> -->
       </el-card>
       <!--数据-->
-      <el-card>24</el-card>
+      <el-card shadow="hover" style="height:522px; margin-top:20px;">24</el-card>
     </el-col>
     <el-col :span="16">
       <div class="num">
-        <el-card v-for="item in 6" :key="item"> 
+        <el-card shadow="hover" v-for="item in 6" :key="item">
           <i class="icon" icon="el-icon-picture"></i>
           <div class="detail">
-            <p class="nums">￥</p>
-            <p class="txt">100</p>
+            <p class="nums">￥1234</p>
+            <p class="txt">今日支付订单</p>
           </div>
         </el-card>
       </div>
       <!--echart-->
-      <el-card>echart</el-card>
+      <el-card shadow="hover">
+        <div style="height:280px"></div>
+      </el-card>
       <!--图-->
       <div class="graph">
-        <el-card class="l-graph">l</el-card>
-        <el-card class="r-graph">r</el-card>
+        <el-card class="l-graph">
+          <div style="height:260px"></div>
+        </el-card>
+        <el-card class="r-graph">
+          <div style="height:260px"></div>
+        </el-card>
       </div>
     </el-col>
   </el-row>
@@ -57,21 +63,33 @@ export default {
 </script>
 
 <style scoped>
-.num{
+.home {
+  /* background: #000; */
+}
+.num {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
 }
-.el-card{
+.num .el-card {
   width: 32%;
   margin-bottom: 20px;
 }
 .icon {
-      font-size: 30px;
-      width: 80px;
-      height: 80px;
-      text-align: center;
-      line-height: 80px;
-      color: #fff;
-    }
+  font-size: 30px;
+  width: 80px;
+  height: 80px;
+  text-align: center;
+  line-height: 80px;
+  color: #fff;
+}
+.graph {
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.graph .el-card {
+  width: 48%;
+}
 </style>
