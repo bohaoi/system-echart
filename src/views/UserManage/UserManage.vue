0ed25fc47b94d6db24a37ba2134d25298138f3fc
bohaoi <bohao8485@163.com>
2020-07-08 15:52:18 +0800

    first submit  form

--- src/views/UserManage/UserManage.vue
+++ src/views/UserManage/UserManage.vue
@@ -1,13 +1,50 @@
 <template>
-  <div class="userManage">usermanage</div>
+  <div class="manage">
+    <div class="manage-header">
+      <!--表头-->
+      <el-button type="primary">+新增</el-button>
+      <common-form inline :formLabel="formLabel" :form="searchFrom">
+        <el-button type="primary">搜索</el-button>
+      </common-form>
+    </div>
+    <common-table></common-table>
+  </div>
 </template>
 
 <script>
-export default {
+import CommonForm from "../../components/CommonForm";
+import CommonTable from "../../components/CommonTable";
 
-}
+export default {
+  components: {
+    CommonForm,
+    CommonTable
+  },
+  data() {
+    return {
+      searchFrom: {
+        keyword: ""
+      },
+      formLabel: [
+        {
+          model: "keyword",
+          label: ""
+        }
+      ]
+    };
+  }
+};
 </script>
 
-<style>
-
+<style scoped>
+.manage {
+  height: 90%;
+  padding-bottom: 20px;
+  overflow: hidden;
+}
+.manage-header {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+}
 </style>
