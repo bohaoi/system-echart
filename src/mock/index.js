d4ec5efa4f4aa0853b88bf63fe9fc0c27a4accf7
bohaoi <bohao8485@163.com>
2020-07-11 11:27:29 +0800

    first submit table

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,5 +1,6 @@
 import Mock from 'mockjs'
 import homeApi from './home.js'
+import userApi from './user'
 
 //设置200-2000毫秒延迟
 Mock.setup({
@@ -9,3 +10,6 @@ Mock.setup({
 //首页相关
 //拦截/home/getData
 Mock.mock(/\/home\/getData/,'get',homeApi.getStatisticalData)
+
+// 用户相关
+Mock.mock(/\/user\/getUser/, 'get', userApi.getUserList)
--- src/views/UserManage/UserManage.vue
+++ src/views/UserManage/UserManage.vue
@@ -7,7 +7,12 @@
         <el-button type="primary">搜索</el-button>
       </common-form>
     </div>
-    <common-table></common-table>
+    <common-table
+      :tableData="tableData"
+      :tableLabel="tableLabel"
+      :config="config"
+      @changePage="getList()"
+    ></common-table>
   </div>
 </template>
 
@@ -22,6 +27,34 @@ export default {
   },
   data() {
     return {
+      tableData: [],
+      tableLabel: [
+        {
+          prop: "name",
+          label: "姓名"
+        },
+        {
+          prop: "age",
+          label: "年龄"
+        },
+        {
+          prop: "sexLabel",
+          label: "性别"
+        },
+        {
+          prop: "birth",
+          label: "出生日期"
+        },
+        {
+          prop: "addr",
+          label: "地址"
+        }
+      ],
+      config: {
+        page: 1,
+        total: 10,
+        loading: false
+      },
       searchFrom: {
         keyword: ""
       },
@@ -32,6 +65,30 @@ export default {
         }
       ]
     };
+  },
+  methods: {
+    getList() {
+      this.config.loading = true;
+      this.$http
+        .get('/api/user/getUser', {
+          params: {
+            page: this.config.page,
+            name
+          }
+        })
+        .then(res => {
+          console.log(res)
+          this.tableData = res.data.list.map(item => {
+            item.sexLabel = item.sex === 0 ? '女' : '男'
+            return item
+          })
+          this.config.total = res.data.count
+          this.config.loading = false
+        })
+    }
+  },
+  created() {
+    this.getList();
   }
 };
 </script>
